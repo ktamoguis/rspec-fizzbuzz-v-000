39ecb97d8913f755c6bbffa09c8ed525e0bcb2ba
Karl Amoguis <ktamoguis@gmail.com>
2018-04-19 00:47:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(user_input)
     "Fizz"
   elsif user_input % 5 == 0
     "Buzz"
-  elsif user_input % 3 == 0 && user_input % 5 == 0
+  elsif user_input % 15 == 0
     "FizzBuzz"
   end
 end
