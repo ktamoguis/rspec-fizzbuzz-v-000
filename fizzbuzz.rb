29c74d6ea058debaba0ffe9ef63d52f542e09134
Karl Amoguis <ktamoguis@gmail.com>
2018-04-19 00:42:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(user_input)
-  if user_input.to_i == 3
-    puts "Fizz"
-  elsif user_input.to_i == 5
-    puts "Buzz"
-  elsif user_input.to_i == 15
-    puts "FizzBuzz"
+  if user_input % 3 == 0
+    "Fizz"
+  elsif user_input % 5 == 0
+    "Buzz"
+  elsif user_input % 15 == 0
+    "FizzBuzz"
   else
 
 end
